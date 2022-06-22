e22ab7acb9a5b0a5e21ad30a328c5fd239d12008
matheus216 <mrt.matheus.dev@gmail.com>
2022-06-22 20:32:35 -0300

    Primeiro commit app

---
+++ src/screens/welcome/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+export const Welcome = () => { 
+    return ( 
+        <View>
+            <Text>MasmasFood</Text>
+        </View>
+    )
+}
